d9199abbbd09ae05eb8e80380d4b91b88e2d8a65
MiguelDevz <174758605+MiguelDevz@users.noreply.github.com>
2025-01-31 16:10:43 -0800

    created circleshape.py file w/circle class

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -11,6 +13,9 @@ def main():
         
         screen.fill("black")
         pygame.display.flip()
+
+        #limits fps to 60
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
