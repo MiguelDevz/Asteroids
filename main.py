96f5e01e77d5ce0b1020fda4432f96e7d69cafde
MiguelDevz <174758605+MiguelDevz@users.noreply.github.com>
2025-02-03 14:15:39 -0800

    updated constants.py and main.py/created player class and file

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
         screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
 
         #limits fps to 60
