ee85e8a963982bb4ec20b399495b65688f2bd7b1
MiguelDevz <174758605+MiguelDevz@users.noreply.github.com>
2025-02-27 15:04:12 -0800

    added pygame Groups

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
         super().__init__(x, y, radius=PLAYER_RADIUS)
         self.rotation = 0
 
+
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -14,16 +15,14 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+    
 
     def draw(self, screen):
         return pygame.draw.polygon(screen, "white", self.triangle(), 2)
     
     def rotate(self, dt):
-        self.rotation = PLAYER_TURN_SPEED * dt
+        self.rotation += PLAYER_TURN_SPEED * dt
 
-    def move(self, dt):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        self.position += forward * PLAYER_SPEED * dt
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -35,7 +34,15 @@ class Player(CircleShape):
             self.rotate(dt)
 
         if keys[pygame.K_w]:
-            self.rotate(dt)
+            self.move(dt)
 
         if keys[pygame.K_s]:
-            self.rotate(dt)
+            self.move(-dt)
+
+
+    
+
+            
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
